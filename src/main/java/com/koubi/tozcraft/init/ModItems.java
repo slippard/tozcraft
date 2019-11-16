1580560636c9e17fb1412cc4396a80d1d8033553
Shane <s.lippard@protonmail.com>
2019-11-16 08:11:47 -0500

    add obsidian ingot

--- src/main/java/com/koubi/tozcraft/init/ModItems.java
+++ src/main/java/com/koubi/tozcraft/init/ModItems.java
@@ -10,4 +10,5 @@ import net.minecraft.item.Item;
 public class ModItems {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	public static final Item RUBY = new ItemBase("ruby");
+	public static final Item OBSIDIAN_INGOT = new ItemBase("obsidian_ingot");
 }
